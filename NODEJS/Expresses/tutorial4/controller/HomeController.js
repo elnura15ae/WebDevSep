17c1211c2ce3c91dd1a783888cc571a431784b53
elnura15ae <elnura15ae@gmail.com>
2021-01-22 10:23:29 +0100

    updated

--- NODEJS/Expresses/tutorial4/controller/HomeController.js
+++
@@ -1,7 +0,0 @@
-const HomeController =(req, res)=>{
-    res.render('index', {
-        name:"jack"
-    })
-}
-
-module.exports={HomeController}
--- NODEJS/Expresses/tutorial4/readme.txt
+++
@@ -1,2 +0,0 @@
-npm install express body-parser node-fetch ejs
-npm i cors
