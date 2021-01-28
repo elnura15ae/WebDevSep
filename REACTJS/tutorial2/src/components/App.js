76a8d160094e1c6c9857ca1c1a42defee70e8db4
elnura15ae <elnura15ae@gmail.com>
2021-01-28 09:36:13 +0100

    updated

--- REACTJS/tutorial2/src/components/App.js
+++ REACTJS/tutorial2/src/components/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import childComponents from './childComponents'
 
 export default class App extends Component {
     constructor(props){
         super(props)
+        this.state={
+            username:'Arne',
+            password:'1234',
+            age:12,
+            scores:[1,2,3,4],
+            nestedObj:{
+                test:[1,2]
+            },
+            myFunction:()=>console.log('test')
+        }
     }
     render() {
         return (
             <div>
                 {console.log(this.props.data)}
+                <childComponents username="Adnan" />
+                {console.log(this.state)}
+                {this.state.myFunction()}
             </div>
         )
     }
---
+++ REACTJS/tutorial2/src/components/childComponents.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class childComponents extends Component {
+    render() {
+        return (
+            <div>
+                Hello {this.props.username}
+            </div>
+        )
+    }
+}
