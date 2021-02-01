0c12cb519fd4bf7c4816b0afaa71255eede9c361
elnura15ae <elnura15ae@gmail.com>
2021-02-01 19:35:51 +0100

    updated

--- REACTJS/Weather/cities/src/App.js
+++ REACTJS/Weather/cities/src/App.js
@@ -7,18 +7,24 @@ const API_KEY = "e3c72586aebd8ff49f894e2f133d874b"
 
 export default class App extends Component {
   
-  getWeather = async () =>{
-     const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`)
-  }
+  getWeather = async (e) =>{
+    e.preventDefault();
+    const city=<e className="taret elements city value"></e>
+     const api_url = await 
+     fetch(`api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`)
+    const data= await api_url.json();
+    console.log(data)
+    }
   
   render() {
     return (
       <div>
-        <Info/>
-        <Form/>
-        <Weather/>
+        <Info />
+        <Form weatherMethod={this.getWeather}/>
+        <Weather />
       </div>
     )
   }
 }
+
 
--- REACTJS/Weather/cities/src/components/form.js
+++ REACTJS/Weather/cities/src/components/form.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export default class Form extends Component {
     render() {
         return (
-            <div>
-                Form components
-            </div>
+            <form onSubmit={this.props.weatherMethod}>
+                <input type="text" name="city" placeholder="enter city"/>
+                <button>show the weather</button>
+            </form>
         )
     }
 }
