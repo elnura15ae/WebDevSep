186e1c4f90dfb579996b12bd2f0642233b010e4d
elnura15ae <elnura15ae@gmail.com>
2021-02-09 01:15:29 +0100

    updated

--- REACTJS/reactjs-ssr/src/App.js
+++ REACTJS/reactjs-ssr/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
-import './App.jcss';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Users from './users'
 import ContactUs from './components/contactForm';
